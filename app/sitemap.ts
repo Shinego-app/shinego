4d9a66247b39e2c6f832749720b93568f25bc8ef
Shinego-app <intercastbv@hotmail.com>
2026-09-25 10:43:16 +0200

    Add Over ons to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -54,6 +54,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/over-ons`,
+      lastModified,
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
       url: `${baseUrl}/veelgestelde-vragen`,
       lastModified,
       changeFrequency: "monthly",
